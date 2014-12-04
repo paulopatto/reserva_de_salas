0e7a892737cbb759f98c71e77846c347c27ec84e
Paulo Patto <paulopatto@gmail.com>
2014-12-04 19:21:40 -0200

    [development] WIP: Hibernate

--- src/main/java/app/model/Person.java
+++ src/main/java/app/model/Person.java
@@ -1,13 +1,32 @@
 package app.model;
-
 ///   vim: ft=java
+
+import java.sql.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 public abstract class Person {
-  private int id;
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @Column(name="name")
   private String name;
+
+  @Column(name="email")
   private String email;
+
+  @Column(name="mobile")
   private String mobile;
 
-  public int getId(){ return this.id; }
+  private Date createdAt;
+  private Date updatedAt;
+
+  public Long getId(){ return this.id; }
 
   public void setName(String name){ this.name = name;}
   public String getName(){ return this.name; }
@@ -17,4 +36,7 @@ public abstract class Person {
 
   public void setMobile(String mobile){ this.mobile = mobile;}
   public String getMobile(){ return this.mobile; }
+
+  public Date getCreatedAt(){ return this.createdAt; }
+  public date getUpdatedAt(){ return this.updatedAt; }
 }
